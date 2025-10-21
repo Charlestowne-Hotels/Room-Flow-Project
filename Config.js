9a7aa1af07de85be1ded97ff42e1a527b16c1105
jryan-CH <jryan@charlestownehotels.com>
2025-10-21 09:41:27 -0400

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCv0LUNSnF_2P9Y13g-x41H4RcN2OiLHVk",
+  authDomain: "room-flow-project.firebaseapp.com",
+  projectId: "room-flow-project",
+  storageBucket: "room-flow-project.firebasestorage.app",
+  messagingSenderId: "904446958639",
+  appId: "1:904446958639:web:d322700bc221d84d22bada",
+  measurementId: "G-8NJ2J6P9EG"
+};
