d3005b14da88bf029d8b8549071c4d820a3a0a94
jryan-CH <jryan@charlestownehotels.com>
2026-02-04 09:19:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2316,4 +2316,3 @@ window.handleSaveLeadTime = async function() {
   } catch (error) { alert("Save failed: " + error.message); } 
   finally { btn.disabled = false; btn.textContent = "Save to Cloud"; }
 };
-
